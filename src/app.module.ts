e0f505dedfaf927e44cf1053561fc875e44b5f8d
leventea <leventea@tuta.io>
2023-03-14 13:12:41 +0100

    Make config global

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
