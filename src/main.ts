4d4f0278a72e83a2653b5f3efc97d4705a7a0c03
leventea <leventea@tuta.io>
2023-03-14 14:55:51 +0100

    Swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const swaggerConf = new DocumentBuilder()
+    .setTitle('Backend Beadando')
+    .setVersion('0.1.0')
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConf);
+  SwaggerModule.setup('api', app, swaggerDoc);
+  
   await app.listen(3000);
 }
 
