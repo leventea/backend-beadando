bbe104b62a8a0e705cba0e29903dfd58cecb1ca5
leventea <leventea@tuta.io>
2023-03-14 15:33:55 +0100

    Database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ToDoModule } from './to-do/to-do.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true
-  }), ToDoModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), 
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        type: 'postgres',
+        host: cfg.get('PG_HOST'),
+        port: cfg.get<number>('PG_PORT'),
+        username: cfg.get('PG_USER'),
+        password: cfg.get('PG_PASS'),
+        database: cfg.get('PG_DB'),
+        synchronize: true,
+        autoLoadEntities: true
+      })
+    }), ToDoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/to-do/entities/to-do.entity.ts
+++ src/to-do/entities/to-do.entity.ts
@@ -1,1 +1,22 @@
-export class ToDo {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ToDo {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    content: string;
+
+    @Column({ nullable: true })
+    location: string;
+
+    @Column()
+    added: Date;
+
+    @Column()
+    deadline: Date;
+}
