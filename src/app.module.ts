9f356000d64f42415bc8d4f1167b53c15eb0d82c
leventea <leventea@tuta.io>
2023-03-14 14:39:15 +0100

    ToDo resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ToDoModule } from './to-do/to-do.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true
-  })],
+  }), ToDoModule],
   controllers: [AppController],
   providers: [AppService],
 })
